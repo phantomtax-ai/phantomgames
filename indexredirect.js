7f5b266deca737da01d8eea3acdbaf2bd94fcb71
Rohith <rohith.palaiya@icloud.com>
2025-06-08 11:50:51 -0400

    Update indexredirect.js

--- indexredirect.js
+++ indexredirect.js
@@ -1,4 +1,4 @@
-function openPage('phantom-games.netlify.app') {
+function openPage('phantom-games.netlify.app/index.html') {
     let newTab = window.open('about:blank'); // Open an empty tab first
-    newTab.location.href = 'phantom-games.netlify.app'; // Then redirect it to the desired page
+    newTab.location.href = 'phantom-games.netlify.app/index.html'; // Then redirect it to the desired page
 }
