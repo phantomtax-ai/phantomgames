8e6422fbc26693a069e08246bf2e4a7d3595d561
Rohith <rohith.palaiya@icloud.com>
2025-06-08 11:38:37 -0400

    Update indexredirect.js

--- indexredirect.js
+++ indexredirect.js
@@ -1,4 +1,4 @@
-function openPage() {
-    let newTab = window.open('about:blank');
-    newTab.location.href = 'index.html';
+function openPage('index.html') {
+    let newTab = window.open('about:blank'); // Open an empty tab first
+    newTab.location.href = 'index.html'; // Then redirect it to the desired page
 }
