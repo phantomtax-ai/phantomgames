8e13e4be4a95862f588504aff97f4abdb66d8176
Rohith <rohith.palaiya@icloud.com>
2025-06-10 08:09:39 -0400

    Delete indexredirect.js

--- indexredirect.js
+++
@@ -1,4 +0,0 @@
-function openPage('phantom-games.netlify.app/index.html') {
-    let newTab = window.open('about:blank'); // Open an empty tab first
-    newTab.location.href = 'phantom-games.netlify.app/index.html'; // Then redirect it to the desired page
-}
